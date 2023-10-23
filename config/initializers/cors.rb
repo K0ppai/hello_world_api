9bfd603d7808cf87bcb7cd54be11252a25342682
K0ppai <psthu.koppai@gmail.com>
2023-10-23 23:31:07 +0630

    Fix rubocop errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,8 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:5173' # Replace with the actual origin of your Vite app
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
